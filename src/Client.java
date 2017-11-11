5ada868eb8226486910bebe06ad7b3312a26afb0
Daniel Klock <danielkl@stud.ntnu.no>
2017-11-11 07:09:12 +0100

    Client changes. Refactor much of the code in the client's main function into other functions. Implement (initial) of an interactive CLI mode for the client.

--- src/Client.java
+++ src/Client.java
@@ -4,50 +4,85 @@ import no.flatfiles.Decompress;
 import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 /**
  * @author Joppe
  * @author Daniel Klock
  * @version 0.1.0
  */
-public class Client {
-    private static final int COMPRESS = 0;
-    private static final int DECOMPRESS = 1;
+public final class Client {
+    private static final List<File> files = new ArrayList<>();
+    private static Mode MODE = Mode.COMPRESS;
 
-    public static void main(String[] args) {
-        List<File> files = new ArrayList<>();
-        int mode = COMPRESS;
+    /**
+     * Give the user a interactive CLI mode.
+     */
+    private static void cliMode() {
+        System.out.println("Welcome to FlatFile's ultimate compression tool\u2122.\n" +
+                           "Please select one of the options below:\n" +
+                           "[1] - Compress files.\n" +
+                           "[2] - Decompress files.\n" +
+                           "[3] - Exit.\n" +
+                           "Option: ");
+        Scanner scanner = new Scanner(System.in);
+        int option = scanner.nextInt();
+        
+    }
 
-        if (args.length == 0) {
-            System.out.println("No args");
-        } else {
-            for (String arg : args) {
-                // Set mode for compression or decompression.
-                if (arg.equals("decompress")) {
-                    mode = DECOMPRESS;
+    /**
+     * Parse commandline arguments.
+     * @param args - The arguments from the CLI.
+     */
+    private static void parseArgs(String[] args) {
+        for (String arg : args) {
+            // Set mode for compression or decompression.
+            if (arg.equals("decompress")) {
+                MODE = Mode.DECOMPRESS;
                 // Create list of files to compress or decompress.
-                } else if (arg.matches("^.+\\..+$")) {
-                    File file = new File(arg);
-                    if (file.canRead()) {
-                        files.add(file);
-                    }
+            } else if (arg.matches("^.+\\..+$")) {
+                File file = new File(arg);
+                if (file.canRead()) {
+                    files.add(file);
                 }
             }
         }
+        executeAction(MODE);
+    }
 
-        switch (mode) {
-            case COMPRESS: {
-                for (File file : files) {
-                    Compress.compress(file);
-                }
-                break;
-            }
+    /**
+     * Compress or Decompress all files currently within the files array.
+     * @param action - Compress or Decompress files.
+     */
+    private static void executeAction(Mode action) {
+        switch (action) {
             case DECOMPRESS: {
                 for (File file : files) {
-                    Decompress.decompress(file);
+                    // TODO: Rewrite Decompress class.
+//                    Decompress.decompress(file);
                 }
                 break;
             }
+            default: {
+                for (File file : files) {
+                    // TODO: Rewrite Compress class.
+//                    Compress.compress(file);
+                }
+                break;
+            }
+        }
+    }
+
+    /**
+     * Give the user an interactive CLI mode, or execute all commands given
+     * through commandline arguments.
+     * @param args - Arguments given through the commandline.
+     */
+    public static void main(String[] args) {
+        if (args.length == 0) {
+            cliMode();
+        } else {
+            parseArgs(args);
         }
     }
 }
---
+++ src/Mode.java
@@ -0,0 +1,15 @@
+/**
+ * @author Daniel Klock
+ * @version 0.1.0
+ */
+public enum Mode {
+    /**
+     * Sets the client to compress given files.
+     */
+    COMPRESS(),
+
+    /**
+     * Sets the client to decompress given files.
+     */
+    DECOMPRESS();
+}
