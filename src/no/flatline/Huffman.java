5d764f9bb8df35d3a9aab0089eeb13cd9f818942
Rolv-Arild Braaten <rolv_arild@hotmail.com>
2017-11-11 12:26:07 +0100

    Added getTree method

--- src/no/flatline/Huffman.java
+++ src/no/flatline/Huffman.java
@@ -1,6 +1,7 @@
 package no.flatline;
 
 import java.io.File;
+import java.util.PriorityQueue;
 
 /**
  * @author Rolv-Arild Braaten
@@ -40,21 +41,66 @@ public class Huffman implements Compressor {
     }
 
     /**
+     * Returns the root of the corresponding Huffman Tree to a string
+     *
+     * @param s the string to get the Huffman Tree for.
+     * @return the root of the corresponding Huffman Tree to {@code s}
+     */
+    private Node getTree(String s) {
+        char[] chars = s.toCharArray();
+        int[] freq = new int[Character.MAX_VALUE]; // one entry for each possible character
+        int diffCharCount = 0; // so priority queue doesn't need to expand later
+        for (char c : chars) {
+            if (freq[c] == 0) diffCharCount++;
+            freq[c]++;
+        }
+        PriorityQueue<Node> nodes = new PriorityQueue<>(diffCharCount);
+        for (char c = 0; c < freq.length; c++) {
+            if (freq[c] != 0) {
+                nodes.add(new Node(c, freq[c]));
+            }
+        }
+        while (nodes.size() > 1) {
+            Node left = nodes.poll();
+            Node right = nodes.poll();
+            int combinedFreq = left.freq;
+            if (right != null) combinedFreq += right.freq;
+            Node link = new Node(combinedFreq, left, right);
+            nodes.add(link);
+        }
+        return nodes.poll();
+    }
+
+    /**
+     * The Node class is used to create a Huffman Tree.
+     * Each node stores a character and its frequency.
+     *
      * @author Joakim Sæther
+     * @author Rolv-Arild Braaten
      * @since 0.0.1
      */
-    private class Node implements Comparable<Node>{
+    private class Node implements Comparable<Node> {
+
+        private static final char LINK_CHAR = 0;
 
         final char character;
         final int freq;
         final Node leftChild;
         final Node rightChild;
 
-        Node(final char character, final int freq, final Node leftChild, final Node rightChild){
+        private Node(final char character, final int freq, final Node leftChild, final Node rightChild){
             this.character = character;
             this.freq = freq;
             this.leftChild = leftChild;
             this.rightChild = rightChild;
+        }
+
+        Node(final int character, final int freq) {
+            this(LINK_CHAR, freq, null, null);
+        }
+
+        Node(final int freq, final Node leftChild, final Node rightChild) {
+            this(LINK_CHAR, freq, leftChild, rightChild);
         }
 
         boolean isLeaf(){
@@ -64,7 +110,7 @@ public class Huffman implements Compressor {
         @Override
         public int compareTo(final Node o){
             final int freqCompare = Integer.compare(this.freq, o.freq);
-            if(freqCompare != 0)return freqCompare;
+            if (freqCompare != 0) return freqCompare;
             return Integer.compare(this.character, o.character);
         }
     }
